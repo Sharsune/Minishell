0c0159d377920e9a29bd0dc851e03b4d43eb1033
Samuel Harsunen <samuel.harsunen@gmail.com>
2023-04-17 18:06:25 +0300

    school headers and fix

--- testmain.c
+++ testmain.c
@@ -1,48 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testmain.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharsune <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:27:50 by sharsune          #+#    #+#             */
+/*   Updated: 2023/04/17 15:27:54 by sharsune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-void signal_handler(int signum) 
+
+void	signal_handler(int signum)
 {
-    printf("\n");
+	printf("\n");
 }
 
-void    signal_quit(int signum)
+void	signal_quit(int signum)
 {
-    printf("\n");
-    exit(0);
+	printf("\n");
+	exit(0);
 }
 
-int main() 
+int	main(void)
 {
-    // Install the signal handler for SIGINT (Ctrl-C)
-    if (signal(SIGINT, signal_handler) == SIG_ERR)
-    {
-        printf("Error installing signal handler\n");
-        return 1;
-    }
+	char	buffer[1024];
+	int		n;
 
-    // Install the signal handler for SIGTSTP (Ctrl-Z)
-    if (signal(SIGQUIT, signal_quit) == SIG_ERR) 
-    {
-        printf("Error installing signal handler\n");
-        return 1;
-    }
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+	{
+		printf("Error installing signal handler\n");
+		return (1);
+	}
+	if (signal(SIGQUIT, signal_quit) == SIG_ERR)
+	{
+		printf("Error installing signal handler\n");
+		return (1);
+	}
 
-    // Wait for signals
-    char buffer[1024];
-    while (1) {
-        write(STDOUT_FILENO, "> ", 2);  // Display prompt
-        ssize_t n = read(STDIN_FILENO, buffer, sizeof(buffer));
-        if (n > 0) {
-            buffer[n] = '\0'; // Add null terminator to end of input string
-            printf("You entered: %s", buffer); // Do something with input
-        } else {
-            printf("\n"); // End of input, exit program
-            return 0;
-        }
-    }
-
-    return 0;
+	while (1)
+	{
+		write(STDOUT_FILENO, "> ", 2);
+		n = read(STDIN_FILENO, buffer, sizeof(buffer));
+		if (n > 0)
+		{
+			buffer[n] = '\0';
+			printf("You entered: %s", buffer);
+		}
+		else
+		{
+			printf("\n");
+			return (0);
+		}
+	}
+	return (0);
 }
