d1730f5f9f21c5c9961f386a21c7f65798e6d362
Samuel Harsunen <samppa@Samuels-MacBook-Air.local>
2023-04-24 02:11:42 +0300

    some builtin stuff

---
+++ builtins.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//vaihda null ja 0 getcwd:stä jos ei saa käyttää?? NULL kertoo että getcwdn pitää itse allocoida meille buffer
+// ja 0 käskee sen itse tehdä oikean kokoisen
+// https://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/g/getcwd.html
+
+int built_pwd() 
+{
+    char *cwd = getcwd(NULL, 0);
+    if (cwd == NULL)
+	{
+    	perror("getcwd");
+		free(cwd);
+        exit(1);
+    }
+    printf("%s\n", cwd);
+    free(cwd);
+    return 0;
+}
+
+//lisätään: parsetaan argumentit ja chdir pyydettyyn pathiin.
+
+int	built_cd(char **argv)
+{
+    if (chdir(argv[1]) != 0) 
+	{
+        perror("chdir");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
+
